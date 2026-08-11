0129735c386a8dd8d2d905afeb5726b41224dda9
Ismari <ismarifuentes28@gmail.com>
2026-08-10 19:24:45 -0600

    Se agrega la entidad Stock_Producto

--- ESFE.GestionProducto.EN/Stock_Producto.cs
+++ ESFE.GestionProducto.EN/Stock_Producto.cs
@@ -4,7 +4,12 @@ using System.Text;
 
 namespace ESFE.GestionProducto.EN
 {
-    internal class Stock_Producto
+    public class Stock_Producto
     {
+        public short IdStock_ProductoPK { get; set; }
+        public short? IdProductoFK { get; set; }
+        public short? Stock { get; set; }
+        public short? Stock_Minimo { get; set; }
+        public bool? Estado { get; set; }
     }
 }
