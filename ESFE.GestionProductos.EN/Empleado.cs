972bbf7fe7204a6dee4a033596316837de759e8c
Ismari <ismarifuentes28@gmail.com>
2026-08-11 07:51:51 -0600

    Empleado

---
+++ ESFE.GestionProductos.EN/Empleado.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ESFE.GestionProductos.EN
+{
+    public class Empleado
+    {
+        public short IdEmpleadoPK { get; set; }
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+        public short? Cargo { get; set; }
+        public short? IdUsuarioFK { get; set; }
+        public bool? Estado { get; set; }
+
+    }
+}
