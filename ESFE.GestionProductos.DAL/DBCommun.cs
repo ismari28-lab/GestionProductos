77061ec8e404b987cde6a9c0ce7b1b6c1304adbf
Josias Ramirez <rzgiovanny22@gmail.com>
2026-08-10 21:27:51 -0600

    Entidad Usuario

--- ESFE.GestionProductos.DAL/DBCommun.cs
+++ ESFE.GestionProductos.DAL/DBCommun.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
+using Microsoft.Data.SqlClient; 
 
-namespace ESFE.GestionProductos.DAL
+namespace ESFE.SysDesarrollo.DL
 {
-    public class DBCommun
+    public class DBComun
     {
+        // Agregamos 'TrustServerCertificate=True' para evitar errores de certificado en LocalDB
+        public const string stringCnn = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BDDesarrollo;Integrated Security=True;TrustServerCertificate=True;";
+
+        /// <summary>
+        /// Crea y retorna una conexión abierta a la base de datos SQL Server.
+        /// </summary>
+        public static IDbConnection ObtenerConexion()
+        {
+            SqlConnection cnn = new SqlConnection(stringCnn);
+            cnn.Open();
+            return cnn;
+        }
     }
 }
---
+++ ESFE.GestionProductos.EN/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ESFE.GestionProductos.EN
+{
+    public class Usuario
+    {
+        [Key]
+        public int IdUsuarioPK { get; set; }
+
+        [StringLength(100)]
+        public string? Nombre { get; set; }
+
+        [StringLength(256)]
+        public string? Password { get; set; }
+
+        public short? Id_RolFK { get; set; }
+
+        public bool? Estado { get; set; }
+    }
+}
