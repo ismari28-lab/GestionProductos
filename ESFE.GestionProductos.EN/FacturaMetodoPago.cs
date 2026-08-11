1ba81bc78da336401d14dbfe8d6581d15910c9ee
Gabriela García <gabrielamrtnz014@gmail.com>
2026-08-11 08:13:59 -0600

    FacturaMetodoPago

---
+++ ESFE.GestionProductos.EN/FacturaMetodoPago.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ESFE.GestionProductos.EN
+{
+    public class FacturaMetodoPago
+    {
+        public short IdPK { get; set; }
+
+        public short? IdFacturaFK { get; set; }
+
+        public short? IdMetodoPagoFK { get; set; } 
+
+        public double? Monto_Cancelado { get; set; }    
+
+        public bool? Estado {  get; set; }
+
+    }
+}
