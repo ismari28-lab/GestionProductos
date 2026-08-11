ba3666a0a9b452941ac853484a2ce86d6e0171c6
Ismari <ismarifuentes28@gmail.com>
2026-08-11 08:02:50 -0600

    MetodoPago

---
+++ ESFE.GestionProductos.EN/MetodoPago.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ESFE.GestionProductos.EN
+{
+    public class MetodoPago
+    {
+        public short IdMetodoPagoPK { get; set; }
+        public string Nombre { get; set; }
+        public bool? Estado { get; set; }
+
+    }
+}
