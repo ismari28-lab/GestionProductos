2c8fde264426dcb3b1e02f3b95be55b71338e01e
Ismari <ismarifuentes28@gmail.com>
2026-08-11 08:06:04 -0600

    Stock_Producto

---
+++ ESFE.GestionProductos.EN/Stock_Producto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ESFE.GestionProductos.EN
+{
+    public class Stock_Producto
+    {
+        public short IdStock_ProductoPK { get; set; }
+        public short? IdProductoFK { get; set; }
+        public short? Stock { get; set; }
+        public short? Stock_Minimo { get; set; }
+        public bool? Estado { get; set; }
+
+    }
+}
