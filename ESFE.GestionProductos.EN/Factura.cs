c4675644f4ee7a7e5625009d9724961f100b27eb
Ismari <ismarifuentes28@gmail.com>
2026-08-11 07:56:48 -0600

    Factura

---
+++ ESFE.GestionProductos.EN/Factura.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ESFE.GestionProductos.EN
+{
+    internal class Factura
+    {
+        public short IdFacturaPK { get; set; }
+        public DateTime? Fecha { get; set; }
+        public short? IdClienteFK { get; set; }
+        public short? IdUsuarioFK { get; set; }
+        public short? Estado { get; set; }
+        public double? Total { get; set; }
+    }
+}
