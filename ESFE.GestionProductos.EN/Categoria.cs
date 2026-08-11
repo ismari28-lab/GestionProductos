100f922ba133cfc2a7dd68d66d114b194f185e9d
Josias Ramirez <rzgiovanny22@gmail.com>
2026-08-10 21:57:46 -0600

    Categoria

---
+++ ESFE.GestionProductos.EN/Categoria.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ESFE.GestionProductos.EN
+{
+    public class Categoria
+    {
+        [Key]
+        public short IdCategoriaPK { get; set; }
+
+        [StringLength(100)]
+        public string? Nombre { get; set; }
+
+        [StringLength(255)]
+        public string? Descripcion { get; set; }
+
+        public bool? Estado { get; set; }
+    }
+}
