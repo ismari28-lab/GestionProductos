bb479d508efcb82ef7e2113a0731f609fe57d98d
Gabriela García <gabrielamrtnz014@gmail.com>
2026-08-11 08:55:50 -0600

    Rol

--- ESFE.GestionProductos.EN/MovimientoInventario.cs
+++ ESFE.GestionProductos.EN/MovimientoInventario.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
 
 namespace ESFE.GestionProductos.EN
 {
-    internal class MovimientoInventario
+    public class MovimientoInventario
     {
+        public short IdMovimientoPK { get; set; }   
+
+        public string? TipoMovimiento { get; set; } 
+
+        public string? Referencia { get; set; }
+
+        public DateTime? Fecha { get; set; }
+
+        public short? IdUsuarioFK { get; set; } 
+
+        public short? Estado { get; set; }
+
     }
 }
