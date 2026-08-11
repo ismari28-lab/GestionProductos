f2ce61919c55ee93fb8e2d5172d49aba1bcc65a2
Josias Ramirez <rzgiovanny22@gmail.com>
2026-08-11 08:25:11 -0600

    Program2

---
+++ ESFE.GestionProductos.UI/Program.cs
@@ -0,0 +1,13 @@
+namespace ESFE.GestionProductos.UI
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            // Asegúrate de que apunte a tu formulario de login
+            Application.Run(new login());
+        }
+    }
+}
