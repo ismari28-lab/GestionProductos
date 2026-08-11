87d8b8ddc1c0077b240e9d7fb1b1f9b602be7035
Gabriela García <gabrielamrtnz014@gmail.com>
2026-08-11 08:52:50 -0600

    Rol

---
+++ ESFE.GestionProductos.EN/Rol.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ESFE.GestionProductos.EN
+{
+    public class Rol
+    {
+        public short IdRolPK { get; set; }
+
+        public string? NombreRol { get; set; }
+
+    }
+}
