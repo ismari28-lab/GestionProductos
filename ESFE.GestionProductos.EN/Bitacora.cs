5c5204570a56445968f01bad4f243132271aed72
Ismari <ismarifuentes28@gmail.com>
2026-08-11 07:47:19 -0600

    Bitacora

--- ESFE.GestionProductos.EN/Bitacora.cs
+++ ESFE.GestionProductos.EN/Bitacora.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ESFE.GestionProductos.EN
 {
-    internal class Bitacora
+    public class Bitacora
     {
+         
+        public short IdBitacoraPK { get; set; }
+        public short? IdUsuarioFK { get; set; }
+        public string Accion { get; set; }
+        public DateTime? Fecha { get; set; }
+        public string Modulo { get; set; }
+        public short? IdRegistroAfectado { get; set; }
     }
 }
+    
