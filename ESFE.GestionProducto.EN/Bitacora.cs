387c0dc90dc685bec4fa15dbbd30f9e4e59c30d8
Ismari <ismarifuentes28@gmail.com>
2026-08-10 18:52:04 -0600

    Se agrega la entidad Bitacora

---
+++ ESFE.GestionProducto.EN/Bitacora.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ESFE.GestionProducto.EN
+{
+    public class Bitacora
+    {
+        public short IdBitacoraPK { get; set; }
+        public short? IdUsuarioFK { get; set; }
+        public string Accion { get; set; }
+        public DateTime? Fecha { get; set; }
+        public string Modulo { get; set; }
+        public short? IdRegistroAfectado { get; set; }
+    }
+}
