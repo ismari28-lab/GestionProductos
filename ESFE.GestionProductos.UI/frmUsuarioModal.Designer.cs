3bd8f096e1129da45bbafde467f08e03e0c1ed55
Ismari <ismarifuentes28@gmail.com>
2026-08-13 11:16:48 -0600

    Modal>

--- ESFE.GestionProductos.UI/frmUsuarioModal.Designer.cs
+++ ESFE.GestionProductos.UI/frmUsuarioModal.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class frmUsuarioModal
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,20 +13,231 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Código generado por el Diseñador de Windows Forms
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmUsuarioModal";
+            materialCard1 = new MaterialSkin.Controls.MaterialCard();
+            txtNombre = new MaterialSkin.Controls.MaterialMaskedTextBox();
+            txtPassword = new MaterialSkin.Controls.MaterialMaskedTextBox();
+            cmbRol = new MaterialSkin.Controls.MaterialComboBox();
+            chkEstado = new MaterialSkin.Controls.MaterialCheckbox();
+            btnCancelar = new MaterialSkin.Controls.MaterialButton();
+            btnGuardar = new MaterialSkin.Controls.MaterialButton();
+            materialCard1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // materialCard1
+            // 
+            materialCard1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            materialCard1.BackColor = Color.FromArgb(255, 255, 255);
+            materialCard1.Controls.Add(btnGuardar);
+            materialCard1.Controls.Add(btnCancelar);
+            materialCard1.Controls.Add(chkEstado);
+            materialCard1.Controls.Add(cmbRol);
+            materialCard1.Controls.Add(txtPassword);
+            materialCard1.Controls.Add(txtNombre);
+            materialCard1.Depth = 0;
+            materialCard1.ForeColor = Color.FromArgb(222, 0, 0, 0);
+            materialCard1.Location = new Point(20, 85); 
+            materialCard1.Margin = new Padding(14);
+            materialCard1.MouseState = MaterialSkin.MouseState.HOVER;
+            materialCard1.Name = "materialCard1";
+            materialCard1.Padding = new Padding(24);
+            materialCard1.Size = new Size(610, 460); // Ajustada la altura para que no se salga de la pantalla
+            materialCard1.TabIndex = 0;
+            // 
+            // txtNombre
+            // 
+            txtNombre.AllowPromptAsInput = true;
+            txtNombre.AnimateReadOnly = false;
+            txtNombre.AsciiOnly = false;
+            txtNombre.BackgroundImageLayout = ImageLayout.None;
+            txtNombre.BeepOnError = false;
+            txtNombre.CutCopyMaskFormat = MaskFormat.IncludeLiterals;
+            txtNombre.Depth = 0;
+            txtNombre.Font = new Font("Microsoft Sans Serif", 16F, FontStyle.Regular, GraphicsUnit.Pixel);
+            txtNombre.HidePromptOnLeave = false;
+            txtNombre.HideSelection = true;
+            txtNombre.Hint = "Nombre de Usuario";
+            txtNombre.InsertKeyMode = InsertKeyMode.Default;
+            txtNombre.LeadingIcon = null;
+            txtNombre.Location = new Point(24, 25);
+            txtNombre.Mask = "";
+            txtNombre.MaxLength = 100;
+            txtNombre.MouseState = MaterialSkin.MouseState.OUT;
+            txtNombre.Name = "txtNombre";
+            txtNombre.PasswordChar = '\0';
+            txtNombre.PrefixSuffixText = null;
+            txtNombre.PromptChar = '_';
+            txtNombre.ReadOnly = false;
+            txtNombre.RejectInputOnFirstFailure = false;
+            txtNombre.ResetOnPrompt = true;
+            txtNombre.ResetOnSpace = true;
+            txtNombre.RightToLeft = RightToLeft.No;
+            txtNombre.SelectedText = "";
+            txtNombre.SelectionLength = 0;
+            txtNombre.SelectionStart = 0;
+            txtNombre.ShortcutsEnabled = true;
+            txtNombre.Size = new Size(560, 55);
+            txtNombre.SkipLiterals = true;
+            txtNombre.TabIndex = 0;
+            txtNombre.TabStop = false;
+            txtNombre.TextAlign = HorizontalAlignment.Left;
+            txtNombre.TextMaskFormat = MaskFormat.IncludeLiterals;
+            txtNombre.TrailingIcon = null;
+            txtNombre.UseSystemPasswordChar = false;
+            txtNombre.ValidatingType = null;
+            // 
+            // txtPassword
+            // 
+            txtPassword.AllowPromptAsInput = true;
+            txtPassword.AnimateReadOnly = false;
+            txtPassword.AsciiOnly = false;
+            txtPassword.BackgroundImageLayout = ImageLayout.None;
+            txtPassword.BeepOnError = false;
+            txtPassword.CutCopyMaskFormat = MaskFormat.IncludeLiterals;
+            txtPassword.Depth = 0;
+            txtPassword.Font = new Font("Microsoft Sans Serif", 16F, FontStyle.Regular, GraphicsUnit.Pixel);
+            txtPassword.HidePromptOnLeave = false;
+            txtPassword.HideSelection = true;
+            txtPassword.Hint = "Contraseña";
+            txtPassword.InsertKeyMode = InsertKeyMode.Default;
+            txtPassword.LeadingIcon = null;
+            txtPassword.Location = new Point(24, 105);
+            txtPassword.Mask = "";
+            txtPassword.MaxLength = 256;
+            txtPassword.MouseState = MaterialSkin.MouseState.OUT;
+            txtPassword.Name = "txtPassword";
+            txtPassword.PasswordChar = '●';
+            txtPassword.PrefixSuffixText = null;
+            txtPassword.PromptChar = '_';
+            txtPassword.ReadOnly = false;
+            txtPassword.RejectInputOnFirstFailure = false;
+            txtPassword.ResetOnPrompt = true;
+            txtPassword.ResetOnSpace = true;
+            txtPassword.RightToLeft = RightToLeft.No;
+            txtPassword.SelectedText = "";
+            txtPassword.SelectionLength = 0;
+            txtPassword.SelectionStart = 0;
+            txtPassword.ShortcutsEnabled = true;
+            txtPassword.Size = new Size(560, 55);
+            txtPassword.SkipLiterals = true;
+            txtPassword.TabIndex = 1;
+            txtPassword.TabStop = false;
+            txtPassword.TextAlign = HorizontalAlignment.Left;
+            txtPassword.TextMaskFormat = MaskFormat.IncludeLiterals;
+            txtPassword.TrailingIcon = null;
+            txtPassword.UseSystemPasswordChar = true;
+            txtPassword.ValidatingType = null;
+            // 
+            // cmbRol
+            // 
+            cmbRol.AutoResize = false;
+            cmbRol.BackColor = Color.FromArgb(255, 255, 255);
+            cmbRol.Depth = 0;
+            cmbRol.DrawMode = DrawMode.OwnerDrawVariable;
+            cmbRol.DropDownHeight = 174;
+            cmbRol.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbRol.DropDownWidth = 121;
+            cmbRol.Font = new Font("Microsoft Sans Serif", 14F, FontStyle.Bold, GraphicsUnit.Pixel);
+            cmbRol.ForeColor = Color.FromArgb(222, 0, 0, 0);
+            cmbRol.FormattingEnabled = true;
+            cmbRol.Hint = "Rol del Usuario";
+            cmbRol.IntegralHeight = false;
+            cmbRol.ItemHeight = 43;
+            cmbRol.Location = new Point(24, 185);
+            cmbRol.MaxDropDownItems = 4;
+            cmbRol.MouseState = MaterialSkin.MouseState.OUT;
+            cmbRol.Name = "cmbRol";
+            cmbRol.Size = new Size(560, 49);
+            cmbRol.StartIndex = 0;
+            cmbRol.TabIndex = 2;
+            // 
+            // chkEstado
+            // 
+            chkEstado.AutoSize = true;
+            chkEstado.Checked = true;
+            chkEstado.CheckState = CheckState.Checked;
+            chkEstado.Depth = 0;
+            chkEstado.Font = new Font("Roboto", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            chkEstado.Location = new Point(24, 260);
+            chkEstado.Margin = new Padding(0);
+            chkEstado.MouseLocation = new Point(-1, -1);
+            chkEstado.MouseState = MaterialSkin.MouseState.HOVER;
+            chkEstado.Name = "chkEstado";
+            chkEstado.Ripple = true;
+            chkEstado.Size = new Size(101, 37);
+            chkEstado.TabIndex = 3;
+            chkEstado.Text = "Activo";
+            chkEstado.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            btnCancelar.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnCancelar.AutoSize = false;
+            btnCancelar.Cursor = Cursors.Hand;
+            btnCancelar.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            btnCancelar.Depth = 0;
+            btnCancelar.HighEmphasis = false;
+            btnCancelar.Icon = null;
+            btnCancelar.Location = new Point(266, 380); // Posición reajustada hacia arriba
+            btnCancelar.Margin = new Padding(4, 6, 15, 6);
+            btnCancelar.MouseState = MaterialSkin.MouseState.HOVER;
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.NoAccentTextColor = Color.Empty;
+            btnCancelar.Size = new Size(150, 48);
+            btnCancelar.TabIndex = 4;
+            btnCancelar.Text = "CANCELAR";
+            btnCancelar.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Outlined;
+            btnCancelar.UseAccentColor = false;
+            btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardar
+            // 
+            btnGuardar.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnGuardar.AutoSize = false;
+            btnGuardar.Cursor = Cursors.Hand;
+            btnGuardar.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            btnGuardar.Depth = 0;
+            btnGuardar.HighEmphasis = true;
+            btnGuardar.Icon = null;
+            btnGuardar.Location = new Point(436, 380); // Posición reajustada hacia arriba
+            btnGuardar.Margin = new Padding(4, 6, 4, 6);
+            btnGuardar.MouseState = MaterialSkin.MouseState.HOVER;
+            btnGuardar.Name = "btnGuardar";
+            btnGuardar.NoAccentTextColor = Color.Empty;
+            btnGuardar.Size = new Size(150, 48);
+            btnGuardar.TabIndex = 5;
+            btnGuardar.Text = "GUARDAR";
+            btnGuardar.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            btnGuardar.UseAccentColor = false;
+            btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // frmUsuarioModal
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(650, 570); // Tamaño total del modal optimizado
+            Controls.Add(materialCard1);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "frmUsuarioModal";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Crear Usuario";
+            materialCard1.ResumeLayout(false);
+            materialCard1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private MaterialSkin.Controls.MaterialCard materialCard1;
+        private MaterialSkin.Controls.MaterialMaskedTextBox txtNombre;
+        private MaterialSkin.Controls.MaterialMaskedTextBox txtPassword;
+        private MaterialSkin.Controls.MaterialComboBox cmbRol;
+        private MaterialSkin.Controls.MaterialCheckbox chkEstado;
+        private MaterialSkin.Controls.MaterialButton btnCancelar;
+        private MaterialSkin.Controls.MaterialButton btnGuardar;
     }
 }
