e28deef426a252d700b0ae8bad84d9ecbb0060a4
Gabriela García <gabrielamrtnz014@gmail.com>
2026-08-18 15:22:35 -0600

    ESFE.GestionProuctos.DAL

---
+++ ESFE.GestionProductos.DAL/UserDAL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using ESFE.SysDesarrollo.DAL; // Asegúrate de importar el namespace de tu DBComun
+using System.Text;
+
+namespace ESFE.GestionProductos.DAL
+{
+    public class UserDAL
+    {
+        public DataTable ListarUsuarios()
+        {
+            DataTable dt = new DataTable();
+            using (IDbConnection conexion = DBComun.ObtenerConexion())
+            {
+                conexion.Open();
+                using (IDbCommand comando = conexion.CreateCommand())
+                {
+                    comando.CommandText = "SP_ListarUsuarios";
+                    comando.CommandType = CommandType.StoredProcedure;
+                    using (IDataAdapter adaptador = new System.Data.SqlClient.SqlDataAdapter(comando as System.Data.SqlClient.SqlCommand))
+                    {
+                        adaptador.Fill(dt);
+                    }
+                }
+            }
+            return dt;
+        }
+    }
+}
