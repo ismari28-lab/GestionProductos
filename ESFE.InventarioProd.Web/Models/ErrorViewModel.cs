594c14a62a20959959db19de9e6049b3b1f0927a
Josias Ramirez <rzgiovanny22@gmail.com>
2026-09-07 10:49:58 -0600

    Web

---
+++ ESFE.InventarioProd.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ESFE.InventarioProd.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
