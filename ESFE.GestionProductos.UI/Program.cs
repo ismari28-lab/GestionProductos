0f00ec7fd532799f08c67f0f8386fb154748d910
Josias Ramirez <rzgiovanny22@gmail.com>
2026-09-07 11:04:08 -0600

    Fix no se de que

--- ESFE.GestionProductos.UI/Program.cs
+++ ESFE.GestionProductos.UI/Program.cs
@@ -10,15 +10,8 @@ namespace ESFE.GestionProductos.UI
         {
             ApplicationConfiguration.Initialize();
 
-<<<<<<< HEAD
-            // Ejecuta directamente el formulario Login
-            Application.Run(new Login());
-=======
-            // Ejecuta directamente el formulario FrmCodigoAcceso
-
             Application.Run(new login());
 
->>>>>>> a598363946559c193b4282da516b8b04c3b1754d
         }
     }
 }
